54ab0ece749ca340a529f4ac56dfb9e2ba8d259e
Daniel Esteves <daniel@seeed.us>
2021-04-29 19:56:01 -0400

    feat(navigator): add support for navigator from global

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { Nothing, isNothing } from 'nothing-mock';
 
 const win = global.window;
 const doc = global.document;
+const nav = global.navigator;
 
-export const window: Window = typeof win !== 'undefined' ? win : Nothing;
 export const document: Document = typeof doc !== 'undefined' ? doc : Nothing;
 export const exists = (variable: any) => !isNothing(variable);
+export const navigator: Navigator = typeof nav !== 'undefined' ? nav : Nothing;
+export const window: Window = typeof win !== 'undefined' ? win : Nothing;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import { document, exists, window } from '../src';
+import { document, exists, navigator, window } from '../src';
 
 describe('window', () => {
   it('should window object exist', () => {
@@ -9,5 +9,11 @@ describe('window', () => {
 describe('document', () => {
   it('should document object exist', () => {
     expect(exists(document)).toBeTruthy();
+  });
+});
+
+describe('navigator', () => {
+  it('should navigator object exist', () => {
+    expect(exists(navigator)).toBeTruthy();
   });
 });
